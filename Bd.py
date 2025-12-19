de4d468f6fd9bc171f0f0fbca584e7bbedc9efca
MeboxT3R1A <mebox.t3r1a@gmail.com>
2025-12-19 10:25:22 -0300

    (gitignore aplicado)

--- Bd.py
+++ Bd.py
@@ -4,7 +4,7 @@ def get_connection():
     conn = mysql.connector.connect(
         host="localhost",
         user="root",
-        port=3307,
+        port=3306,
         password="senac",
         database="bd_diplomas"
     )
